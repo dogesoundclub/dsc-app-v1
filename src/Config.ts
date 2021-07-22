ac9d57c736e30da5c17739ae71542b2b0c5b77ee
Hanul <hanul@hanul.me>
2021-07-22 20:56:28 +0900

    버그 수정된 슬로건 계약 반영

--- src/Config.ts
+++ src/Config.ts
@@ -8,7 +8,7 @@ export default {
     contracts: {
         // Baobab
         Mate: "0xCa7894953066B8C0a05e123454f586CDe4B42Fa7",
-        Slogan: "0xBdf6fB7862e6c5f5273157A6fB542Efaec32d2AF",
+        Slogan: "0x5BE7BFee608b2aa0c022D11EFDCCC0bD3477C6Cf",
         // Cypress
         //Mate: "",
         //Slogan: "",
