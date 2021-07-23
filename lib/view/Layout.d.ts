462096525eb012fe7f015cd6e7ab3f9001f3ebe1
Hanul <hanul@hanul.me>
2021-07-23 13:59:46 +0900

    모바일 홈 화면 구현 완료

--- lib/view/Layout.d.ts
+++ lib/view/Layout.d.ts
@@ -1,5 +1,9 @@
 import { Component } from "react";
-export default class Layout extends Component {
+export default class Layout extends Component<{}, {
+    showingNav: boolean;
+}> {
+    constructor(props: {});
+    private clickMenuButtonHandler;
     render(): JSX.Element;
 }
 //# sourceMappingURL=Layout.d.ts.map
--- src/view/Layout.tsx
+++ src/view/Layout.tsx
@@ -1,21 +1,33 @@
 import { Component } from "react";
 import BrowserInfo from "../BrowserInfo";
 
-export default class Layout extends Component {
+export default class Layout extends Component<{}, {
+    showingNav: boolean,
+}> {
+
+    constructor(props: {}) {
+        super(props);
+        this.state = { showingNav: false };
+    }
+
+    private clickMenuButtonHandler = () => {
+        this.setState({ showingNav: this.state.showingNav !== true });
+    };
 
     public render() {
         return <>
-            <header>
+            <header className={this.state.showingNav === true ? "showing" : "hiding"}>
                 <a href="/">HOME</a>
                 <a href="/mate">NFT</a>
                 <a href="/gallary">GALLARY</a>
                 <a href="/activities">ACTIVITIES</a>
                 <a className="ready" href="/">BOARD</a>
+                <div className="language">● LANG : <select value={BrowserInfo.language.substring(0, 2)} onChange={(event) => BrowserInfo.changeLanguage(event.target.value)}>
+                    <option value="ko">한국어</option>
+                    <option value="en">English</option>
+                </select></div>
             </header>
-            <div className="language">● language : <select value={BrowserInfo.language.substring(0, 2)} onChange={(event) => BrowserInfo.changeLanguage(event.target.value)}>
-                <option value="ko">한국어</option>
-                <option value="en">English</option>
-            </select></div>
+            <a className="menu-button" onClick={this.clickMenuButtonHandler}><img src="/images/menu-button.png" srcSet="/images/menu-button@2x.png 2x" /></a>
             {this.props.children}
             <footer>
                 © 2021 DSLAB<br />
