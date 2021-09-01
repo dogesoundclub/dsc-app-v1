db8bb93b26ef353fede4e5aee534c0d1d6e54949
Hanul <hanul@hanul.me>
2021-09-01 23:30:02 +0900

    지갑이 연동되는 경우에는 지갑을 사용하도록 개선

--- src/klaytn/Klaytn.ts
+++ src/klaytn/Klaytn.ts
@@ -2,15 +2,18 @@ import Config from "../Config";
 
 class Klaytn {
 
-    private caver = new (window as any).Caver(new (window as any).Caver.providers.HttpProvider("https://node-api.klaytnapi.com/v1/klaytn", {
-        headers: [
-            { name: "Authorization", value: Config.authorization },
-            { name: "x-chain-id", value: String(Config.chainId) },
-        ],
-    }));
+    private caver = (window as any).caver !== undefined ? undefined :
+        new (window as any).Caver(new (window as any).Caver.providers.HttpProvider("https://node-api.klaytnapi.com/v1/klaytn", {
+            headers: [
+                { name: "Authorization", value: Config.authorization },
+                { name: "x-chain-id", value: String(Config.chainId) },
+            ],
+        }));
 
     public createContract(address: string, abi: any) {
-        return new this.caver.contract.create(abi, address);
+        return this.caver === undefined ?
+            new (window as any).caver.klay.Contract(abi, address) :
+            this.caver.contract.create(abi, address);
     }
 }
 
