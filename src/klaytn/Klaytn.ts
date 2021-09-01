ca516f39fc675b3ee49d22da74a767fba9ea8368
Hanul <hanul@hanul.me>
2021-09-01 19:38:42 +0900

    주소 변경

--- src/klaytn/Klaytn.ts
+++ src/klaytn/Klaytn.ts
@@ -1,6 +1,11 @@
 class Klaytn {
 
-    private caver = new (window as any).Caver("https://kaikas.cypress.klaytn.net:8651/");
+    private caver = new (window as any).Caver(new (window as any).Caver.providers.HttpProvider("https://node-api.klaytnapi.com/v1/klaytn", {
+        headers: [
+            { name: "Authorization", value: "Basic S0FTUDA4STVDRkFQTkc4TkdaMVhMRk03OmkxT1RkZ3dMU1BOMWxVbHRjc0FUM1Q4YVg4RjNISzluUVJMbTBMMXA=" },
+            { name: "x-chain-id", value: "8217" },
+        ],
+    }));
 
     public createContract(address: string, abi: any) {
         return new this.caver.contract.create(abi, address);
