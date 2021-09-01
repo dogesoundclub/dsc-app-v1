a706b32e85fce45ac4eb02fd2e9502df97c1caf3
Hanul <hanul@hanul.me>
2021-09-01 19:43:16 +0900

    헤더 변경

--- src/klaytn/Klaytn.ts
+++ src/klaytn/Klaytn.ts
@@ -2,7 +2,7 @@ class Klaytn {
 
     private caver = new (window as any).Caver(new (window as any).Caver.providers.HttpProvider("https://node-api.klaytnapi.com/v1/klaytn", {
         headers: [
-            { name: "Authorization", value: "Basic S0FTUDA4STVDRkFQTkc4TkdaMVhMRk03OmkxT1RkZ3dMU1BOMWxVbHRjc0FUM1Q4YVg4RjNISzluUVJMbTBMMXA=" },
+            { name: "Authorization", value: "Basic S0FTS0VNMEpVNVZZSjYwTFRMRk1VVlYzOlRTeHlHbnF2dDM1TlJuN2thU0NZdm1FN1JsMjZYdndGVG5yb25fWEw=" },
             { name: "x-chain-id", value: "8217" },
         ],
     }));
